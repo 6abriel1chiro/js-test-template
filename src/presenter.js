60422a8a9084791b1a9e1627e49490935529c4b6
Gabriel Ichiro <gabriel582084@gmail.com>
2022-09-26 22:05:32 -0400

    Update presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,10 @@
 import sumar from "./sumador";
-import multiplicar from "./multiplicador";
 import dividir from "./divisor";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-suma-div");
-const div2 = document.querySelector("#resultado-mult-div");
 const div3 = document.querySelector("#resultado-divide-div");
 
 
@@ -17,8 +15,6 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-
-  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
   div3.innerHTML = "<p>" + dividir(firstNumber, secondNumber) + "</p>";
 
 });
